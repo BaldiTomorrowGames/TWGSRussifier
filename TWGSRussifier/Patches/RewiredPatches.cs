48925a7b5179c0ed52d3bdb954e97f58774403dd
NekitTOP4IK <60227820+NekitTOP4IK@users.noreply.github.com>
2025-05-18 12:58:09 +0300

    Removed unused RewiredPatches.cs

--- TWGSRussifier/Patches/RewiredPatches.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TWGSRussifier.Patches
-{
-   // [HarmonyPatch(typeof(Rewired.UI.ControlMapper.ControlMapper))]
-   // [HarmonyPatch("Quit")]
-    public static class RewiredPatches
-    {
-    }
-}
