f2ed40e6081dff5e95f63b31ed3e28d06e181bfa
kanoyo-git <trefford21@gmail.com>
2026-05-04 18:49:09 +0700

    feat: add clickable TWGSRussifier dropdown menu
    Signed-off-by: kanoyo-git <trefford21@gmail.com>

--- TWGSRussifier/Patches/MainMenuPatch.cs
+++ TWGSRussifier/Patches/MainMenuPatch.cs
@@ -2,17 +2,77 @@ using HarmonyLib;
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.Events;
+using UnityEngine.UI;
 using TWGSRussifier.API;
 
 namespace TWGSRussifier.Patches
 {
+    internal class ArrowPositioner : MonoBehaviour
+    {
+        private TextMeshProUGUI textComponent = null!;
+        private RectTransform arrowRect = null!;
+
+        public void Initialize(TextMeshProUGUI text, RectTransform arrow)
+        {
+            textComponent = text;
+            arrowRect = arrow;
+        }
+
+        void LateUpdate()
+        {
+            if (textComponent != null && arrowRect != null)
+            {
+                textComponent.ForceMeshUpdate();
+                var textInfo = textComponent.textInfo;
+
+                if (textInfo.characterCount > 0)
+                {
+                    var lastVisibleCharInfo = textInfo.characterInfo[textInfo.characterCount - 1];
+                    float textEdgeX = lastVisibleCharInfo.topRight.x;
+
+                    arrowRect.anchorMin = new Vector2(0.5f, 0.5f);
+                    arrowRect.anchorMax = new Vector2(0.5f, 0.5f);
+                    arrowRect.pivot = new Vector2(0.5f, 0.5f);
+                    arrowRect.anchoredPosition = new Vector2(textEdgeX + 5f, 2.4f);
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch]
     internal class MainMenuPatch
     {
-        private static bool initialized = false;
-        
         private static readonly Dictionary<string, string> LocalizationKeys = new Dictionary<string, string>()
         {
             { "Version", "TWGS_MainMenu_Version" },
+        };
+
+        private static readonly List<SocialMediaInfo> SocialMediaLinks = new List<SocialMediaInfo>()
+        {
+            new SocialMediaInfo("TelegramButton", "TWGS_Menu_Telegram", "https://t.me/translate_balda"),
+            new SocialMediaInfo("DiscordButton", "TWGS_Menu_Discord", "https://discord.gg/cGmxrmp5Tj"),
+            new SocialMediaInfo("GameBananaButton", "TWGS_Menu_GameBanana", "https://gamebanana.com/mods/597541"),
+            new SocialMediaInfo("YouTubeButton", "TWGS_Menu_YouTube", "https://www.youtube.com/@DragTors"),
+            new SocialMediaInfo("GithubButton", "TWGS_Menu_Github", "https://github.com/BaldiTomorrowGames/TWGSRussifier/tree/bbcr")
+        };
+
+        private class SocialMediaInfo
+        {
+            public string ButtonName { get; private set; }
+            public string LocalizationKey { get; private set; }
+            public string Url { get; private set; }
+
+            public SocialMediaInfo(string buttonName, string localizationKey, string url)
+            {
+                ButtonName = buttonName;
+                LocalizationKey = localizationKey;
+                Url = url;
+            }
+        }
+
+        private static readonly List<KeyValuePair<string, Vector2>> SizeDeltaTargets = new List<KeyValuePair<string, Vector2>>
+        {
         };
 
         [HarmonyPatch(typeof(MainMenu), "Start")]
@@ -21,11 +81,10 @@ namespace TWGSRussifier.Patches
             [HarmonyPostfix]
             private static void Postfix(MainMenu __instance)
             {
-                if (!initialized)
-                {
-                    ApplyLocalizationToMainMenu(__instance.transform);
-                    initialized = true;
-                }
+                ApplySizeChanges(__instance.transform);
+                ApplyLocalization(__instance.transform);
+                CreateModInfoButton(__instance.transform);
+                FixCursorSiblingIndex(__instance.transform);
             }
         }
 
@@ -35,28 +94,249 @@ namespace TWGSRussifier.Patches
             [HarmonyPostfix]
             private static void Postfix(GameObject __instance, bool value)
             {
-                if (__instance.name == "Title" && value && !initialized)
+                if (__instance.name == "Title" && value)
                 {
-                    ApplyLocalizationToMainMenu(__instance.transform);
-                    initialized = true;
+                    ApplySizeChanges(__instance.transform);
+                    ApplyLocalization(__instance.transform);
+                    CreateModInfoButton(__instance.transform);
+                    FixCursorSiblingIndex(__instance.transform);
                 }
             }
         }
 
-        [HarmonyPatch(typeof(CoreGameManager), "ReturnToMenu")]
-        private static class ReturnToMenuPatch
-        {
-            [HarmonyPrefix]
-            private static void Prefix()
-            {
-                initialized = false;
-            }
-        }
+        private static GameObject? socialLinksPanel = null;
+        private static bool dropdownVisible = false;
+        private static RectTransform? dropdownArrow = null;
 
-        private static void ApplyLocalizationToMainMenu(Transform rootTransform)
+        private static void CreateModInfoButton(Transform rootTransform)
         {
             if (rootTransform == null) return;
 
+            Transform? versionTransform = rootTransform.Find("Version");
+            if (versionTransform == null) return;
+
+            Transform? existingModInfo = rootTransform.Find("ModInfo");
+            if (existingModInfo != null) return;
+
+            versionTransform.gameObject.SetActive(false);
+
+            GameObject modInfo = Object.Instantiate(versionTransform.gameObject, rootTransform);
+            modInfo.name = "ModInfo";
+            modInfo.SetActive(true);
+
+            modInfo.transform.localPosition = new Vector3(-180f, 155f, 0f);
+
+            RectTransform? rectTransform = modInfo.GetComponent<RectTransform>();
+            if (rectTransform != null)
+            {
+                rectTransform.sizeDelta = new Vector2(150f, 50f);
+                rectTransform.offsetMin = new Vector2(-239f, 160f);
+            }
+
+            TextMeshProUGUI? textComponent = modInfo.GetComponent<TextMeshProUGUI>();
+            if (textComponent != null)
+            {
+                textComponent.raycastTarget = true;
+
+                TextLocalizer? localizer = textComponent.GetComponent<TextLocalizer>();
+                if (localizer == null)
+                {
+                    localizer = textComponent.gameObject.AddComponent<TextLocalizer>();
+                }
+
+                GameObject arrowObject = new GameObject("DropdownArrow", typeof(RectTransform));
+                arrowObject.transform.SetParent(modInfo.transform, false);
+
+                RectTransform arrowRect = arrowObject.GetComponent<RectTransform>();
+                arrowRect.sizeDelta = new Vector2(8f, 8f);
+
+                modInfo.AddComponent<ArrowPositioner>().Initialize(textComponent, arrowRect);
+
+                GameObject triangle = new GameObject("Triangle", typeof(RectTransform));
+                triangle.transform.SetParent(arrowObject.transform, false);
+
+                RectTransform triangleRect = triangle.GetComponent<RectTransform>();
+                triangleRect.anchorMin = Vector2.zero;
+                triangleRect.anchorMax = Vector2.one;
+                triangleRect.sizeDelta = Vector2.zero;
+
+                TriangleImage triangleUI = triangle.AddComponent<TriangleImage>();
+                triangleUI.color = new Color(0.5f, 0.5f, 0.5f, 1f);
+
+                dropdownArrow = arrowRect;
+
+                localizer.key = "TWGS_Menu_ModInfo";
+                localizer.RefreshLocalization();
+
+                StandardMenuButton button = textComponent.gameObject.GetComponent<StandardMenuButton>();
+                if (button == null)
+                {
+                    button = textComponent.gameObject.AddComponent<StandardMenuButton>();
+                }
+
+                button.OnPress = new UnityEvent();
+                button.OnPress.AddListener(() => { ToggleSocialLinksDropdown(rootTransform, modInfo); });
+                button.OnRelease = new UnityEvent();
+                button.OnHighlight = new UnityEvent();
+                button.OffHighlight = new UnityEvent();
+                button.underlineOnHigh = true;
+                button.text = textComponent;
+
+                try
+                {
+                    modInfo.tag = "Button";
+                }
+                catch
+                {
+                }
+            }
+
+            CreateSocialLinksPanel(rootTransform, modInfo);
+        }
+
+        private static void CreateSocialLinksPanel(Transform rootTransform, GameObject modInfoButton)
+        {
+            GameObject panel = new GameObject("SocialLinksPanel", typeof(RectTransform), typeof(CanvasRenderer), typeof(Image));
+            panel.transform.SetParent(rootTransform, false);
+
+            RectTransform panelRect = panel.GetComponent<RectTransform>();
+            panelRect.anchorMin = new Vector2(0, 1);
+            panelRect.anchorMax = new Vector2(0, 1);
+
+            panelRect.anchoredPosition = new Vector2(145, -116);
+
+            float buttonHeight = 35f;
+            float topBottomPadding = 20f;
+            float panelHeight = (SocialMediaLinks.Count * buttonHeight) + topBottomPadding;
+
+            panelRect.sizeDelta = new Vector2(130f, panelHeight);
+
+            Image panelImage = panel.GetComponent<Image>();
+            panelImage.color = new Color(0.1f, 0.1f, 0.1f, 0.9f);
+
+            for (int i = 0; i < SocialMediaLinks.Count; i++)
+            {
+                SocialMediaInfo info = SocialMediaLinks[i];
+                CreateSocialButton(panel, info.ButtonName, info.LocalizationKey, info.Url, i);
+            }
+
+            panel.SetActive(false);
+            socialLinksPanel = panel;
+        }
+
+        private static void CreateSocialButton(GameObject parent, string buttonName, string localizationKey, string url, int index)
+        {
+            GameObject buttonObj = new GameObject(buttonName, typeof(RectTransform), typeof(TextMeshProUGUI));
+            buttonObj.transform.SetParent(parent.transform, false);
+
+            RectTransform buttonRect = buttonObj.GetComponent<RectTransform>();
+            buttonRect.anchorMin = new Vector2(0, 1);
+            buttonRect.anchorMax = new Vector2(1, 1);
+            buttonRect.pivot = new Vector2(0.5f, 1);
+            buttonRect.anchoredPosition = new Vector3(0, -10 - (index * 35), 0);
+            buttonRect.sizeDelta = new Vector2(0, 30);
+
+            TextMeshProUGUI textComponent = buttonObj.GetComponent<TextMeshProUGUI>();
+            textComponent.fontSize = 16;
+            textComponent.alignment = TextAlignmentOptions.Center;
+            textComponent.raycastTarget = true;
+
+            TextLocalizer localizer = buttonObj.AddComponent<TextLocalizer>();
+
+            if (string.IsNullOrEmpty(localizationKey))
+            {
+                localizationKey = buttonName;
+            }
+
+            localizer.key = localizationKey;
+            localizer.RefreshLocalization();
+
+            StandardMenuButton button = buttonObj.GetComponent<StandardMenuButton>();
+            if (button == null)
+            {
+                button = buttonObj.AddComponent<StandardMenuButton>();
+            }
+
+            button.OnPress = new UnityEvent();
+            button.OnPress.AddListener(() => {
+                Application.OpenURL(url);
+                ToggleSocialLinksDropdown(null, null);
+            });
+            button.OnRelease = new UnityEvent();
+            button.OnHighlight = new UnityEvent();
+            button.OffHighlight = new UnityEvent();
+            button.underlineOnHigh = true;
+            button.text = textComponent;
+
+            try
+            {
+                buttonObj.tag = "Button";
+            }
+            catch
+            {
+            }
+        }
+
+        private static void ToggleSocialLinksDropdown(Transform? rootTransform, GameObject? modInfoButton)
+        {
+            if (socialLinksPanel == null || rootTransform == null || modInfoButton == null)
+            {
+                return;
+            }
+
+            dropdownVisible = !dropdownVisible;
+            socialLinksPanel.SetActive(dropdownVisible);
+
+            if (dropdownArrow != null)
+            {
+                dropdownArrow.localRotation = Quaternion.Euler(0, 0, dropdownVisible ? -90 : 0);
+            }
+        }
+
+        private static void ApplySizeChanges(Transform rootTransform)
+        {
+            foreach (var target in SizeDeltaTargets)
+            {
+                Transform? t = rootTransform.Find(target.Key);
+                if (t != null)
+                {
+                    RectTransform? rt = t.GetComponent<RectTransform>();
+                    if (rt != null)
+                    {
+                        rt.sizeDelta = target.Value;
+                    }
+                }
+            }
+        }
+
+        private class TriangleImage : UnityEngine.UI.Graphic
+        {
+            protected override void OnPopulateMesh(VertexHelper vh)
+            {
+                vh.Clear();
+
+                Vector2 center = rectTransform.rect.center;
+                float width = rectTransform.rect.width;
+                float height = rectTransform.rect.height;
+
+                UIVertex vert = UIVertex.simpleVert;
+                vert.color = color;
+
+                vert.position = new Vector3(center.x + width / 2, center.y, 0);
+                vh.AddVert(vert);
+
+                vert.position = new Vector3(center.x - width / 2, center.y + height / 2, 0);
+                vh.AddVert(vert);
+
+                vert.position = new Vector3(center.x - width / 2, center.y - height / 2, 0);
+                vh.AddVert(vert);
+
+                vh.AddTriangle(0, 1, 2);
+            }
+        }
+
+        private static void ApplyLocalization(Transform rootTransform)
+        {
             foreach (var kvp in LocalizationKeys)
             {
                 string path = kvp.Key;
@@ -80,6 +360,15 @@ namespace TWGSRussifier.Patches
                     }
                 }
             }
+        }
+
+        private static void FixCursorSiblingIndex(Transform rootTransform)
+        {
+            Transform? cursor = rootTransform.Find("CursorOrigin(Clone)");
+            if (cursor == null)
+                cursor = rootTransform.Find("CursorOrigin");
+            if (cursor != null)
+                cursor.SetSiblingIndex(8);
         }
     }
 }
